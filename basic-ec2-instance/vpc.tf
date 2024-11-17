493e1dc722742212c4e017219edfc322b4c59b77
sudovazid <vazidmail@protonmail.com>
2024-11-17 10:53:25 +0530

    Added: Public subnet

--- basic-ec2-instance/vpc.tf
+++ basic-ec2-instance/vpc.tf
@@ -1,7 +1,12 @@
 resource "aws_vpc" "first-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block
 
   tags = {
-    Name = "first-vpc"
+    Name = var.vpc_name
   }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = var.public_cidr_block
 }
